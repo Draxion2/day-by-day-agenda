36714b43620b4185192ff0c94f6a4594be64730c
= <holdanmcmullen@gmail.com>
2022-02-25 00:50:48 -0700

    pseudocode js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// create array for hourly time slots
+// create elements that make up time slots
+// append elements together
+// check due date
+// append elements on the page
+
+// load previous time slots from local storage
+// if nothing, check due date
+
+// save time slots from local storage
+// get date from time slot element
+// convert to moment object by the hour
+
+// remove classes from element
+// apply new classes if time slot is near or over due
+
+// time slot is clicked
+// get current text of textarea element
+// replace textarea element with new input
+
+// update time slot in array and save again to local storage
+// replace textarea with new content
+
+// remove all time slots
+
+// load time slots for the first time
